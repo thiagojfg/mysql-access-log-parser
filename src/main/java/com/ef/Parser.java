982d5f7269dce6a5e7fc05cd21ddfc3ee9cb92c3
Thiago Luiz Alves <thiagojfg@gmail.com>
2018-02-27 16:43:18 -0300

    first commit

---
+++ src/main/java/com/ef/Parser.java
@@ -0,0 +1,10 @@
+package com.ef;
+
+public class Parser {
+
+    public static void main(String[] args) {
+        for (String string : args) {
+            System.out.println(string);
+        }
+    }
+}
