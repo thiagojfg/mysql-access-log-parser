7bc9a6f9cd0e6a51aa9bd03bc8444725e4f1e011
Thiago Luiz Alves <thiagojfg@gmail.com>
2018-02-27 18:28:58 -0300

    add file to .gitignore

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -2,9 +2,16 @@ package com.ef;
 
 public class Parser {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ConfigFailedException {
         
+        Config config = Config.parse(args);
         
+        if(config.getAccessLog() != null){            
+            //load acesslog file to mysql database
+        }
         
+        //search mysql records using params on config file
+        
+        //return results
     }
 }
